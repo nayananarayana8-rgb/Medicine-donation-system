ce3682f80e25e800de395be105f9244ef80ff067
nayananarayana8-rgb <nayananarayana8@gmail.com>
2026-03-25 15:35:03 +0530

    Update database.sql

--- database.sql
+++ database.sql
@@ -1,1 +1,16 @@
 
+CREATE TABLE donors (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name TEXT,
+    phone TEXT,
+    medicine TEXT,
+    quantity INTEGER
+);
+
+CREATE TABLE requests (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name TEXT,
+    phone TEXT,
+    medicine TEXT,
+    quantity INTEGER
+);
